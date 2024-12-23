a5de16d719e8308a0ed6bb7bb0bbc854f1d89161
Romulo Menezes <romulojnr@gmail.com>
2024-12-23 14:48:31 -0300

    recursividade (TODO)

--- 02-intro-tads/05-recursividade/labs/lab1/app.js
+++ 02-intro-tads/05-recursividade/labs/lab1/app.js
@@ -1,22 +1,54 @@
-class NomeDaClasse {
-  // Construtor da classe
-  constructor(parametros) {
-    // Inicialização de propriedades
-    this.propriedade = parametros;
-  }
+// Fatorial com Recursividade
 
-  // Método da classe
-  metodo() {
-    // Código do método
+function fatorial_rep( numero) {
+  if (numero === 0) {
+    return 1
   }
-
-  static metodoEstatico() {
-    // Código do método que pode ser executado diretamente pela classe
+  let total = 1
+  for (let i = 2; i <= numero; i++) {
+    console.log(total)
+    total = total * i
   }
+  console.log(total)
 }
 
-NomeDaClasse.prototype.valor = 10;
-NomeDaClasse.valor = 10;
+function fatorial( numero ){
+  if (numero === 0) {
+    return 1
+  }
+  return numero * fatorial( numero - 1)
+}
 
-const obj = new NomeDaClasse()
-console.log(obj.valor)
+let memo = []
+function fibonacci( mes ) {
+  if (mes === 0  || mes === 1) {
+    return mes
+  }
+
+  if (memo[mes]) {
+    console.log("reutilização do valor: " + mes)
+    return memo[mes]
+  } else {
+    memo[mes] = fibonacci(mes -1) + fibonacci(mes - 2)
+  }
+  console.log(memo)
+  return memo[mes]
+}
+
+// Crie uma função recursiva para obter a multiplicação a partir de operações de soma
+// 4 x 2
+function multiplicacao(n1, n2) {
+  if (n2 == 1) {
+    return n1
+  }
+  return n1 + multiplicacao(n1, n2 - 1)
+}
+// 2³ = 2 * 2² = 2 * 2 * 2¹ 
+function exponenciacao(n1, n2) {
+  if (n2 === 1){
+    return n1
+  }
+  return n1 * exponenciacao(n1, n2-1)
+}
+
+console.log(fibonacci(12))
